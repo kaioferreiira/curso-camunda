7a795288f5839915924a3ee5f9010a524d4259c2
Kaio Ferreira <kaio.costa@ntconsult.com.br>
2021-02-23 12:13:21 -0300

    Exercicio 3b

--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,10 +1,43 @@
 package com.camunda.training;
 
+import static org.camunda.bpm.engine.test.assertions.bpmn.AbstractAssertions.init;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
 public class ProcessJUnitTest {
-  
-//  @Test
+
+
+  @Rule
+  public ProcessEngineRule rule = new ProcessEngineRule();
+
+  @Before
+  public void setup() {
+    init(rule.getProcessEngine());
+  }
+
+  @Test
+  @Deployment(resources = "process.bpmn")
   public void testHappyPath() {
-    
+
+    // Create a HashMap to put in variables for the process instance
+    Map<String, Object> variables = new HashMap<String, Object>();
+    variables.put("approved", true);
+
+    // Start process with Java API and variables
+
+    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("twitter-qa", variables);
+    // Make assertions on the process instance
+    assertThat(processInstance).isEnded();
+
   }
 
 }
