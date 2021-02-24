ebd459c00a5e3507534d746706670dde11937dfc
Kaio Ferreira <kaio.costa@ntconsult.com.br>
2021-02-24 09:19:53 -0300

    Exercicio  4

--- src/main/java/com/camunda/training/delegate/CreateTweetDelegate.java
+++ src/main/java/com/camunda/training/delegate/CreateTweetDelegate.java
@@ -12,12 +12,16 @@ public class CreateTweetDelegate implements JavaDelegate {
   private final Logger LOGGER = LoggerFactory.getLogger(CreateTweetDelegate.class.getName());
 
   public void execute(DelegateExecution execution) throws Exception {
-    String content = "I did it! Kaio here . . .";
+
+    String content = (String) execution.getVariable("content");
+
     LOGGER.info("Publishing tweet: " + content);
     AccessToken accessToken = new AccessToken("220324559-jet1dkzhSOeDWdaclI48z5txJRFLCnLOK45qStvo", "B28Ze8VDucBdiE38aVQqTxOyPc7eHunxBVv7XgGim4say");
     Twitter twitter = new TwitterFactory().getInstance();
     twitter.setOAuthConsumer("lRhS80iIXXQtm6LM03awjvrvk", "gabtxwW8lnSL9yQUNdzAfgBOgIMSRqh7MegQs79GlKVWF36qLS");
     twitter.setOAuthAccessToken(accessToken);
     twitter.updateStatus(content);
+
+
   }
 }
--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -9,7 +9,9 @@ import java.util.Map;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -17,7 +19,10 @@ public class ProcessJUnitTest {
 
 
   @Rule
-  public ProcessEngineRule rule = new ProcessEngineRule();
+  @ClassRule
+//  public ProcessEngineRule rule = new ProcessEngineRule();
+  public static ProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create().build();
+
 
   @Before
   public void setup() {
@@ -31,6 +36,7 @@ public class ProcessJUnitTest {
     // Create a HashMap to put in variables for the process instance
     Map<String, Object> variables = new HashMap<String, Object>();
     variables.put("approved", true);
+    variables.put("content", "Exercise 4 test - Kaio Test:  " + Math.random());
 
     // Start process with Java API and variables
 
